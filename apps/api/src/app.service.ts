6a5ac43a236011a6548ac95a9a767d7d1cd1bbd1
0g3ra <olegyera@gmail.com>
2025-05-14 21:23:52 +0300

    chore: finish integration between api, domain and web

--- apps/api/src/app.service.ts
+++ apps/api/src/app.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { sum } from '@plinks-pw/domain';
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World! ' + sum(2, 2);
   }
 }
